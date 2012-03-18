a6004571e1fa01b51bbb592f80032dccce41b861
MindMirror <wek1004@gmail.com>
2012-03-18 23:39:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5cfce970d40dad81a1a4db17df493caaadb91ad886fe1e5386770c163e65e39dc915cc7790f5ab2b091b1564e2b08da7bfe7a48ee8f43f8f68e433d2cd5a493'
